c36a7366a3d96a2f3534ff1a271375041fd65938
ratnadipbharde <ratnadipbharde@gmail.com>
2022-11-20 15:33:00 +0530

    Added Calculate length of line solution

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,29 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Main obj = new Main();
         obj.displayMessage();
+        System.out.println("Length of Line is : "+obj.getLength());//for calculate length
+    }
+
+    private double getLength() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("For Line 1 1st point :- ");
+        System.out.print("Enter value of x : ");
+        int x1 = sc.nextInt();
+        System.out.print("Enter value of y : ");
+        int y1 = sc.nextInt();
+        System.out.println();
+        System.out.println("For Line 1 2nd point :- ");
+        System.out.print("Enter value of x : ");
+        int x2 = sc.nextInt();
+        System.out.print("Enter value of y : ");
+        int y2 = sc.nextInt();
+        double length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+        return length;
     }
 
     private void displayMessage() {
