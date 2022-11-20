fbf739a42fabb55b9f0e2467cc7557a772b69caa
ratnadipbharde <ratnadipbharde@gmail.com>
2022-11-20 15:16:39 +0530

    Added Displaying Welcome to Line Comparison Computation Program

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Main obj = new Main();
+        obj.displayMessage();
+    }
+
+    private void displayMessage() {
+        System.out.println("Welcome to Line Comparison Program");
+    }
+}
